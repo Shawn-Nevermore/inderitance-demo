3159182d8d008c118c64d3d3dc12f0ecc57ec0fd
Shawn-Nevermore <shawnxu2016sg@gmail.com>
2019-08-06 12:40:18 +0800

    添加ES5 object.create 和 es6 class extends

--- es3-inheritance.js
+++ es3-inheritance.js
@@ -31,7 +31,7 @@ function Human(person) {
     this.birthday = person.birthday || '1970-01-01'
 }
 
-Human.prototype = Object.create(Animal.prototype)
+inherits(Human, Animal)
 
 // 注意，如果写成 Human.prototype = {} 形式，会直接打破原型链，继承会出错
 Human.prototype.species = 'Human'
@@ -44,8 +44,32 @@ function Asian(person) {
     this.city = person.city || 'Beijing'
 }
 
-Asian.prototype = Object.create(Human.prototype)
+inherits(Asian, Human)
 
-Asian.prototype.skin = 'yellow'
+Asian.prototype.skin = 'Yellow'
 
 var jay = new Asian({ name: 'Jay', birthday: '1979-01-18', city: 'Taiwan' })
+
+/****************** Helper ***********************/
+function inherits(Child, Parent) {
+    function F() {}
+    F.prototype = Parent.prototype
+    Child.prototype = new F()
+    Child.prototype.constructor = Child
+}
+
+/*
+
+// 1.空函数 F
+function F() {}
+
+// 2.把 F 的原型指向 Animal.prototype
+F.prototype = Animal.prototype
+
+// 3.把 Human 的原型指向一个新的 F 对象，F 对象的原型正好指向 Animal.prototype
+Human.prototype = new F()
+
+// 4.将 Human 原型的构造函数修复为 Human(Human.prototype被重写,导致Human.prototype.constructor也一同被重写)
+Human.prototype.constructor = Human
+
+*/
---
+++ es6-inheritance.js
@@ -0,0 +1,48 @@
+/**
+ * 要求使用 class，完成如下需求：
+ *
+ * 1. 写出一个构造函数 Animal
+ *    输入为空
+ *    输出为一个新对象，该对象的共有属性为 {行动: function(){}}，没有自有属性
+ * 2. 再写出一个构造函数 Human
+ *    Human 继承 Animal
+ *    输入为一个对象，如 {name: 'Frank', birthday: '2000-10-10'}
+ *    输出为一个新对象，该对象自有的属性有 name、物种和 birthday，共有的属性有行动和使用工具
+ *   （由于 class 的语法问题，所以物种只能勉为其难作为一个自有属性，本来应该是共有属性的）
+ * 3. 再写出一个构造函数 Asian
+ *    Asian 继承 Human
+ *    输入为一个对象，如 {city: '北京', name: 'Frank', birthday: '2000-10-10' }
+ *    输出为一个新对象，改对象自有的属性有 name city 物种 肤色和 bitrhday，共有的属性有行动和使用工具
+ *
+ * 即，最后一个新对象是 Asian 构造出来的，Asian 继承 Human，Human 继承 Animal
+ */
+
+class Animal {
+    move() {
+        console.log('I am moving...')
+    }
+}
+
+class Human extends Animal {
+    constructor(person) {
+        super()
+        this.name = person.name || 'Unnamed'
+        this.birthday = person.birthday || '1970-01-01'
+    }
+
+    toolManipulating() {
+        console.log('I can use tools!')
+    }
+}
+
+Human.prototype.species = 'Human'
+
+class Asian extends Human {
+    constructor(person) {
+        super(person)
+        this.city = person.city || 'Beijing'
+    }
+}
+
+Asian.prototype.skin = 'Yellow'
+var jay = new Asian({ name: 'Jay', birthday: '1979-01-18', city: 'Taiwan' })
